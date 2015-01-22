3207893eb4cb43a5c4f511a27d0ac5e3e4bbe1b1
linostar <linux.anas@gmail.com>
2015-01-22 20:51:58 +0200

    Added quickescape script

---
+++ quickescape.pl
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+use vars qw($VERSION %IRSSI);
+
+use Irssi qw(command_bind signal_add settings_add_str settings_get_str);
+
+$VERSION = '1.00';
+%IRSSI = (authors => 'Linostar',
+          name => 'Quick Escape Script',
+          description => 'Quick Escape from Trap Channels',
+          license => 'New BSD');
+
+sub escape {
+  my ($server, $msg, $nick, $nick_addr, $target) = @_;
+  my $url = '(https://github.com/linostar/irssi-scripts)';
+  my $chan = settings_get_str('quickescape_channel');
+  my $bot = settings_get_str('quickescape_bot');
+  if ($target =~ m/$chan/i && $nick =~ m/$bot/i) {
+    $server->command("part $target Automated leaving script " . $url);
+  }
+}
+
+settings_add_str('quickescape', 'quickescape_channel' => "dontjoinitsatrap");
+settings_add_str('quickescape', 'quickescape_bot' => "trapbot");
+signal_add('message public', 'escape');
+
